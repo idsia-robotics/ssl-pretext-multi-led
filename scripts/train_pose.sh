7a84e936e7e35e10836b0dc63d60e9441b09e414
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-15 11:19:40 +0100

    Fixed python version

--- scripts/train_pose.sh
+++ scripts/train_pose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 training.py -d data/robomaster_ds_train.h5 -t pose\
+python3.11 training.py -d data/robomaster_ds_training.h5 -t pose\
     -n position_test --experiment-id 0\
     --device $1 -a --visible\
     -v data/robomaster_ds_validation.h5
