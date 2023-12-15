21332a2a1a59609db91e03132486816f40cd404b
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-15 11:21:43 +0100

    updated dataset name

--- scripts/train_pose.sh
+++ scripts/train_pose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3.11 training.py -d data/robomaster_ds_train.h5 -t pose\
+python3.11 training.py -d data/robomaster_ds_training.h5 -t pose\
     -n pose_test --experiment-id 0\
     --device $1 -a --visible\
     -v data/robomaster_ds_validation.h5
