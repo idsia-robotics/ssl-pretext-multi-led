2f3c4f9ad8b02b85b2120168d8ccc1ec3ccddd81
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-18 15:32:26 +0100

    Torchscan

--- src/models/summary.py
+++ src/models/summary.py
@@ -1,9 +1,9 @@
 from src.models import model_registry
 
 if __name__ == '__main__':
-    from torchinfo import summary
+    from torchscan import summary
 
     for model in model_registry.values():
         model = model(task = 'pose')
-        summary(model, input_size=(1, 3, 360, 640))
+        summary(model, (3, 360, 640), receptive_field=True)
         print()
