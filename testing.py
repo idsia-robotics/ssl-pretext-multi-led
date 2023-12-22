d16478db679e18a1cd7f3630693c8ef3b66585c6
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-22 16:33:12 +0100

    Better plots, testing and viz.

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import mlflow
 import numpy as np
 from src.config.argument_parser import parse_args
@@ -7,7 +8,7 @@ from torch.utils.data import DataLoader
 import tqdm
 import pandas as pd
 from src.metrics import angle_difference, mse
-
+from datetime import datetime
 from src.models import load_model_mlflow, load_model_raw
 from src.viz.plots import orientation_error_distribution, theta_scatter_plot, proj_scatter_plot, proj_error_distribution, custom_scatter, orientation_error_by_orientation
 from matplotlib import pyplot as plt
@@ -18,7 +19,7 @@ def main():
     ds = get_dataset(args.dataset, camera_robot=args.robot_id, target_robots=[args.target_robot_id],
                      augmentations=args.augmentations, only_visible_robots=args.visible,
                      sample_count=args.sample_count, sample_count_seed=args.sample_count_seed)
-    dataloader = DataLoader(ds, batch_size = 1, shuffle = False)
+    dataloader = DataLoader(ds, batch_size = 64, shuffle = False)
 
     using_mlflow = False
 
@@ -72,8 +73,11 @@ def main():
     # ds = pd.DataFrame(data)
     mean_dist_error = np.abs(data["dist_true"] - data["dist_pred"]).mean()
     mean_angle_error = np.mean(data['theta_error'])
-    mean_proj_erorr = np.median(np.linalg.norm(data["proj_true"] - data["proj_pred"], axis = 1))
-    print(f"Median proj error: {mean_proj_erorr}")
+    median_proj_error = np.median(np.linalg.norm(data["proj_true"] - data["proj_pred"], axis = 1))
+
+    under_30 = np.linalg.norm(data["proj_true"] - data["proj_pred"], axis = 1) < 30
+    precision_30 = under_30.sum() / under_30.shape[0]
+    print(f"Median proj error: {median_proj_error}")
     print(f"Mean distance error: {mean_dist_error}")
     print(f"Mean angle error (rads): {mean_angle_error}")
     print(f"Mean angle error (degs): {np.rad2deg(mean_angle_error)}")
@@ -84,22 +88,29 @@ def main():
         proj_scatter_plot,
         proj_error_distribution,
         orientation_error_distribution,
-        custom_scatter('cos_true', 'cos_pred', 'Cos scatter', xlim = [-1, 1], ylim=[-1,1]),
-        custom_scatter('sin_true', 'sin_pred', 'Sin scatter', xlim = [-1, 1], ylim=[-1,1]),
+        custom_scatter('cos_true', 'cos_pred', 'Cos scatter', xlim = [-1, 1], ylim=[-1,1], plot_name="Cos scatter"),
+        custom_scatter('sin_true', 'sin_pred', 'Sin scatter', xlim = [-1, 1], ylim=[-1,1], plot_name="Sin scatter"),
         orientation_error_by_orientation,
-        custom_scatter('theta_error', 'dist_true', 'Theta error vs Distance', xlabel = "Theta Error [rad]", ylabel = "Distance [m]", correlation=True)
+        custom_scatter('theta_error', 'dist_true', 'Theta error vs Distance', xlabel = "Theta Error [rad]", ylabel = "Distance [m]", correlation=True, plot_name="Distance-Theta error scatter")
     ]
 
     if using_mlflow:
         with mlflow.start_run(run_id=run_id) as run:
+            mlflow.log_metric('testing/proj/mse', median_proj_error)
+            mlflow.log_metric('testing/ori/mae', mean_angle_error)
+            mlflow.log_metric('testing/distance/mae', mean_dist_error)
+            mlflow.log_metric("testing/proj/p30", precision_30)
+
             for fig_fn in figures:
                 fig = fig_fn(data)
                 mlflow.log_figure(fig, fig_fn.__name__)
     else:
+        run_name = datetime.today().isoformat()
+        out_folder = Path("plots/") / run_name
+        out_folder.mkdir(exist_ok=True, parents=True)
         for fig_fn in figures:
             fig = fig_fn(data)
-            fig.show()
-        plt.show()
+            fig.savefig(out_folder / fig_fn.__name__)
             
 
 
