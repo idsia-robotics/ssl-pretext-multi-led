f688e29405427ee6722bc0ccfda046821bd38618
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-27 11:10:48 +0100

    Forgot backslash

--- src/models/summary.py
+++ src/models/summary.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     from torchscan import summary
 
     for model in model_registry.values():
-        model = model(task = 'pose')
+        model = model(task = 'pose_and_led')
         summary(model, (3, 360, 640), receptive_field=True)
         print()
