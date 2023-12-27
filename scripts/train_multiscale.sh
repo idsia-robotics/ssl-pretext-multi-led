608b689318c945ccb5ea61b26c1f0882b87c4a09
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-27 10:58:26 +0100

    Multiscale model

---
+++ scripts/train_multiscale.sh
@@ -0,0 +1,8 @@
+RUN_NAME=multiscale_led_masked
+
+python3.11 -m training -d ../robomaster_led/robomaster_ds_training.h5 -t pose_and_led\
+    -n ${RUN_NAME} --experiment-id 0\
+    --device $1 -a --visible\
+    --model-type multiscale_model_s
+    -v ../robomaster_led/robomaster_ds_validation.h5 --epochs 100 --learning-rate 0.002
+python3.11 -m testing_led --checkpoint-id 99 --run-name ${RUN_NAME} --experiment-id 0 --task pose_and_led --dataset data/robomaster_ds_validation.h5 --device $1
---
+++ src/models/multi_scale_fcn.py
@@ -0,0 +1,68 @@
+import torch
+from src.models import ModelRegistry
+from src.models.fcn import Model_s
+
+@ModelRegistry("multiscale_model_s")
+class MS_Model_s(Model_s):
+    
+    def __init__(self, *args, **kwargs) -> None:
+        super(MS_Model_s, self).__init__(*args, **kwargs)
+        self.downsample_layer = torch.nn.AvgPool2d(2)
+        self.upsample_layer = torch.nn.Upsample((45, 80), mode='bilinear')
+
+        self.core_layers = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 6, kernel_size=3, padding=1, stride=1),
+            torch.nn.BatchNorm2d(6),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(6, 8, kernel_size=3, padding=1, stride=1),
+            torch.nn.BatchNorm2d(8),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(8, 16, kernel_size=3, padding=1, stride=1),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(16, 32, kernel_size=3, padding=1, stride=1),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(32, 32, kernel_size=3, padding=2, stride=1, dilation = 2),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 32, kernel_size=3, padding=2, stride=1, dilation=2),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(),
+            # torch.nn.Conv2d(32, 3, kernel_size=1, padding=0, stride=1),
+
+        )
+
+        self.robot_pose_and_led_layer = torch.nn.Sequential(
+                torch.nn.Conv2d(64, 10, kernel_size=1, padding=0, stride=1),
+                torch.nn.ReLU(),
+                torch.nn.BatchNorm2d(10),
+                torch.nn.Conv2d(10, 10, kernel_size=1, padding=0, stride=1),
+                # torch.nn.Sigmoid()
+            )
+        self.forward = self.forward_fn
+
+    def forward_fn(self, x):
+        downsampled = self.downsample_layer(x)
+        out = self.core_layers(x)
+        downsampled_out = self.core_layers(downsampled)
+        downsampled_out = self.upsample_layer(downsampled_out)
+        concat = torch.cat([out, downsampled_out], dim = 1)
+        out = self.robot_pose_and_led_layer(concat)
+        out = torch.cat(
+            [
+                torch.nn.functional.sigmoid(out[:, :2, ...]), # pos and dist
+                torch.nn.functional.tanh(out[:, 2:4, ...]), # orientation
+                torch.nn.functional.sigmoid(out[:, 4:, ...]), # leds
+                
+            ],
+            axis = 1)
+        out[:, 1, ...] = out[:, 1, ...] * self.MAX_DIST_M
+        return out
+
+    
+
+
