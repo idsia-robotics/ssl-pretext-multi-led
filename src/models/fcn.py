Fixed numerical error on CELoss

src/models/fcn.py
@@ -179,13 +179,13 @@ class Model_s(BaseModel):
         led_trues = batch["led_mask"].to(led_outs.device) # BATCH_SIZE x 6
 
         masked_led_outs = led_outs * torch.tile(pos_trues[:, None, ...], dims = (1,))
-        led_preds = masked_led_outs.sum(axis=[-1, -2])
+        led_preds = torch.clamp(masked_led_outs.sum(axis=[-1, -2]), 0., 1.)
         losses = [0] * 6
         for i in range(led_preds.shape[1]):
             led_mask = batch["led_visibility_mask"].to(model_out.device)[:, i].double()
             losses[i] = (torch.nn.functional.binary_cross_entropy(
-                led_preds[:, i], led_trues[:, i].double(), reduction='none'
-            ) * led_mask / (led_mask.sum() + 1e-15)).sum()
+                    led_preds[:, i], led_trues[:, i].double(), reduction='none'
+                ) * led_mask / (led_mask.sum() + 1e-15)).sum()
         return sum(losses) / 6, losses
 
     