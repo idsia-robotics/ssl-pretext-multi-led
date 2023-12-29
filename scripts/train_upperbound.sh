4bd0375f8b4d521a9dc48a3bc1e19391ac0b174a
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2023-12-29 17:22:42 +0100

    Traininb scripts for baseline, pretext, and UB

---
+++ scripts/train_upperbound.sh
@@ -0,0 +1,8 @@
+RUN_NAME=upperbound
+
+python3.11 -m training -d data/robomaster_ds_training.h5 -t pose_and_led\
+    -n ${RUN_NAME} --experiment-id 0\
+    --device $1 -a --visible\
+    -v data/robomaster_ds_validation.h5 --epochs 100 --learning-rate 0.002\
+    --w-proj .25 --w-dist .25 --w-ori .25 --w-led .25
+python3.11 -m testing_led --checkpoint-id 99 --run-name ${RUN_NAME} --experiment-id 0 --task pose_and_led --dataset data/robomaster_ds_validation.h5 --device $1
