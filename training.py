eb892beba9f9d20588b497dcb07c5d1bf31c13ab
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-01-24 18:06:52 +0100

    <FIX> Removed breakpoint

--- training.py
+++ training.py
@@ -77,7 +77,6 @@ def train_loop(model : BaseModel, train_dataloader, val_dataloader, device,
             loss = unsupervised_loss + supervised_loss
             loss.backward()
             optimizer.step()
-            breakpoint()
 
             losses.append(loss.detach().item())
             p_losses.append(summed_p_loss.detach().item())
