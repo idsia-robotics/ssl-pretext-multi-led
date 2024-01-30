eb6e56c17b0a350f08f77c0800c736ddb18b7b2b
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-01-30 15:50:38 +0100

    <NEW> More parameters on grid search script.

--- scripts/grid_search_train.py
+++ scripts/grid_search_train.py
@@ -6,30 +6,33 @@ from itertools import product
 def main():
     params = OrderedDict({
         'lambda' : [.5,],
-        'sample-count' : [1000,]
+        'sample-count' : [1000,],
+        'train_dataset' : ["top_bottom_ds_training.h5"],
+        'val_dataset' : ["top_bottom_ds_validation.h5"],
+        "model" : ['mobile_net']
     })
 
     for comb in product(*params.values()):
         print(comb)
-        lmb, count = comb
-        run_name = f"mobile_net_sync_l_{lmb}_c_{count}"
+        lmb, count, train_ds, val_ds, model = comb
+        run_name = f"{train_ds}_{model}_sync_l_{lmb}_c_{count}"
         pretext_weight = lmb
         task_weights = (1 - lmb) / 3
 
         cmd = f"""
-python3.11 -m training -d data/top_bottom_ds_training.h5 -t pose_and_led \
+python3.11 -m training -d data/{train_ds} -t pose_and_led \
 -n {run_name} --experiment-id 0 \
 --device cuda:0 -a \
---epochs 100 --learning-rate 0.001 -v data/top_bottom_ds_validation.h5 \
+--epochs 100 --learning-rate 0.001 -v data/{val_ds} \
 --w-proj {task_weights} --w-dist {task_weights} --w-ori {task_weights} --w-led {pretext_weight} \
---labeled-count {count} --labeled-count-seed 0 --model-type mobile_net"""
+--labeled-count {count} --labeled-count-seed 0 --model-type {model}"""
         
         subprocess.run(cmd.strip().split(' '))
 
         testing_cmd = f"""
 python3.11 -m testing --checkpoint-id 99 --run-name {run_name} \
 --experiment-id 0 --task pose_and_led --visible \
---dataset data/top_bottom_ds_validation.h5 --device cuda:0 \
+--dataset data/{val_ds} --device cuda:0 \
         """
         subprocess.run(testing_cmd.strip().split(' '))
 
