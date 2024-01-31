c3f25b0499d85e2c3409b350b386ab0232e6b081
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-01-31 22:45:36 +0100

    Removed HFLIP

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -243,7 +243,7 @@ def get_dataset(dataset_path, camera_robot = None, target_robots = None, augment
     transform = lambda x: x
     if augmentations:
         transform = torchvision.transforms.Compose([
-            RandomHorizontalFlip((360, 640)),
+#            RandomHorizontalFlip((360, 640)),
             RandomRotTranslTransform(9, .1, bound=H5Dataset.POS_ORB_SIZE * 2),
             SimplexNoiseTransform((360, 640)),
             ColorJitterAugmentation(
