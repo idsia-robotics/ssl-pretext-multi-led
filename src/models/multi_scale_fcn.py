e365c30ef0a940466197f6801c153bd3f3b11c22
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-02-13 17:40:43 +0100

    <NEW> Adjusted ms RF

--- src/models/multi_scale_fcn.py
+++ src/models/multi_scale_fcn.py
@@ -1,6 +1,10 @@
+from typing import Any
+import numpy as np
+import scipy
 import torch
 from src.models import ModelRegistry
 from src.models.fcn import Model_s
+from torch.nn import functional as F
 
 class ConvBlock(torch.nn.Module):
     def __init__(self, in_channels = 64) -> None:
@@ -45,10 +49,10 @@ class MS_Model_s(Model_s):
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
-            torch.nn.Conv2d(32, 32, kernel_size=3, padding=2, stride=1, dilation = 2),
+            torch.nn.Conv2d(32, 32, kernel_size=3, padding=1, stride=1),
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
-            torch.nn.Conv2d(32, 32, kernel_size=3, padding=2, stride=1, dilation=2),
+            torch.nn.Conv2d(32, 32, kernel_size=1, padding=0, stride=1),
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
 
@@ -83,4 +87,106 @@ class MS_Model_s(Model_s):
         return out
 
     
+class GaussianLayer(torch.nn.Module):
+    def __init__(self, kernel_size = 3):
+        super(GaussianLayer, self).__init__()
+        self.seq = torch.nn.Sequential(
+            torch.nn.ReflectionPad2d(kernel_size // 2), 
+            torch.nn.Conv2d(3, 3, kernel_size=kernel_size, stride=1, padding=0, bias=None, groups=3)
+        )
+
+        self.kernel_size = kernel_size
+        self.weights_init()
+
+    def forward(self, x):
+        return self.seq(x)
+
+    def weights_init(self):
+        n= np.zeros((self.kernel_size,self.kernel_size))
+        n[self.kernel_size // 2,self.kernel_size // 2] = 1
+        k = scipy.ndimage.gaussian_filter(n,sigma=self.kernel_size // 5)
+        for name, f in self.named_parameters():
+            f.data.copy_(torch.from_numpy(k))
+
+class LaplacianPyramidBlock(torch.nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size = (3,3), padding = 1, stride = 1,
+                 activation_block = torch.nn.ReLU, blur_kernel_size = 3) -> None:
+        
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+            torch.nn.BatchNorm2d(out_channels),
+        )
+
+        self.act_block = activation_block() if activation_block else lambda x: x
+        
+        if stride == 1:
+            self.forward = self._no_skip_impl
+        else:
+            self.kernel = GaussianLayer(blur_kernel_size)
+            self.forward = self._skip_impl
+
+        self.downscaler = torch.nn.AvgPool2d(stride, stride)
+
+    def _no_skip_impl(self, x, image):
+        features = self.layers(x)
+        if image is None:
+            image = x
+        return self.act_block(features), self.downscaler(image).detach()
+
+    def _skip_impl(self, x, image):
+        breakpoint()
+        features = self.layers(x)
+        if image is None:
+            image = x
+        downscaled = self.downscaler(self.kernel(image)).detach()
+        return self.act_block(features + downscaled), downscaled
+
+    # def make_gaussian_kernel(self, kernel_size, sigma):
+    #     ts = torch.linspace(-kernel_size // 2, kernel_size // 2 + 1, kernel_size)
+    #     gauss = torch.exp((-(ts / sigma)**2 / 2))
+    #     kernel = gauss / gauss.sum()
+    #     return kernel
+    
+    # def fast_gaussian_blur(self, img: torch.Tensor, kernel: torch.Tensor) -> torch.Tensor:
+    #     trailing_dims = img.shape[:-3]
+    #     kernel_size = kernel.shape[0]
+
+    #     padding = (left, right, top, bottom)
+    #     padding = [kernel_size // 2, kernel_size // 2, kernel_size // 2, kernel_size // 2]
+    #     img = F.pad(img, padding, mode="constant", value=0)
+
+    #     # Separable 2d conv
+    #     breakpoint()
+    #     kernel = kernel.view(*trailing_dims, 1, kernel_size, 1)
+    #     img = F.conv1d(img, kernel)
+    #     kernel = kernel.view(*trailing_dims, 1, 1, kernel_size)
+    #     img = F.conv1d(img, kernel)
+
+    #     return img
+
+    def __call__(self, *args) -> Any:
+        if len(args) > 1:
+            return self.forward(args[0], args[1])
+        else:
+            return self.forward(args[0], args[0])
+
+
+    
+# @ModelRegistry("laplacian_ms")
+class Laplacian_ms(Model_s):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.layers = torch.nn.Sequential(
+            LaplacianPyramidBlock(in_channels=3, out_channels=8, kernel_size=7, padding=3, stride=1, blur_kernel_size=7),
+            LaplacianPyramidBlock(in_channels=16, out_channels=16, kernel_size=7, padding=3, stride=2, blur_kernel_size=7),
+            LaplacianPyramidBlock(in_channels=16, out_channels=32, kernel_size=5, padding=2, stride=1, blur_kernel_size=5),
+            LaplacianPyramidBlock(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=2, blur_kernel_size=3),
+            LaplacianPyramidBlock(in_channels=32, out_channels=64, kernel_size=3, padding=0, stride=2, blur_kernel_size=3),
+            LaplacianPyramidBlock(in_channels=64, out_channels=10, kernel_size=1, padding=0, stride=1, blur_kernel_size=1),
+        )
+
 
