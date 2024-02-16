a63059885377099556fb09729c6208badac7091b
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-02-16 22:53:41 +0100

    Fix

--- scripts/fine_tune.py
+++ scripts/fine_tune.py
@@ -2,14 +2,14 @@ import subprocess
 
 val_ds = ""
 train_ds = ""
-run_name = ""
+run_name = "fine_tune"
 epochs = 100
 task_weights = .3
 pretext_weight = 0.
 led = "amax"
 size = 64
 count = 100
-run_id="dc073d899ea74a63b0c564c2b8c04b56"
+fine_tune_id="four_leds_model_s_wide_l_1.0_c_1"
 lr_sch = "cosine"
 
 
@@ -19,5 +19,6 @@ python3.11 -m training -d data/{train_ds} -t pose_and_led \
 --device cuda:0 -a \
 --epochs {epochs} --learning-rate 0.001 -v data/{val_ds} \
 --w-proj {task_weights} --w-dist {task_weights} --w-ori {task_weights} --w-led {pretext_weight} \
---labeled-count-seed 0 --led-inference {led} --lr-schedule {lr_sch} --batch-size {size} --visible"""
+--labeled-count-seed 0 --led-inference {led} --lr-schedule {lr_sch} --batch-size {size} --visible \
+--checkpoint-id 99 --run-name {fine_tune_id}"""
 subprocess.run(cmd.strip().split(' '))
