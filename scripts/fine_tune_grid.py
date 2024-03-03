438b70d42e546e483e38e6e0cdb1258b08727033
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-03-03 16:31:24 +0100

    Updated fine tune script

--- scripts/fine_tune_grid.py
+++ scripts/fine_tune_grid.py
@@ -8,11 +8,11 @@ def main():
     device = "cuda:0"
 
     params = OrderedDict({
-        'sample-count' : [10, 100, 1000, None],
+        'sample-count' : [-1, 10, 100, 1000, None],
         'train_dataset' : ["real_four_ds_training.h5"],
         'val_dataset' : ["real_four_ds_validation.h5"],
         "model" : ['model_s_wide'],
-        'replicas' : ["1", "2", "3"],
+        'replicas' : ["1", "2", "3", "4"],
         "use_pre_trained" : [True, False]
     })
 
@@ -20,16 +20,33 @@ def main():
         count, train_ds, val_ds, model, rpl, using_pre_trained = comb
 
         pre_trained_name = "pretext_real_four_leds_attention_pretext"
-        if int(rpl) > 1:
-            pre_trained_name = pre_trained_name + f"_rpl{rpl}"
+        led_inference = "amax"
+        tasks_w = 0.3
+        led_w = 0
 
+        if int(rpl) > 1 and int(rpl) < 4:
+            pre_trained_name = pre_trained_name + f"_rpl{rpl}"
+        elif int(rpl) == 4:
+            pre_trained_name = "pretext_None_rpl4"
+
+        if not count == -1:
+            continue
         if count == None:
-            if int(rpl) == 1:
-                continue
             if using_pre_trained:
                 continue
             run_name = f"upperbound_{rpl}"
             task = "tuning"
+        elif count == -1:
+            if using_pre_trained:
+                continue
+            approach_name = "pretext"
+            task = "pretext"
+            led_inference = "pred"
+            count = None
+            led_w = 1.
+            tasks_w = 0.
+#            run_name = f"{approach_name}_{count}_rpl{rpl}"
+            run_name = pre_trained_name
         else:
             approach_name = "fine_tune" if using_pre_trained else "baseline"
             task = "tuning"
@@ -42,20 +59,20 @@ python3.11 -m training -d data/{train_ds} -t {task} \
 -n {run_name} --experiment-id 0 \
 --device {device} -a \
 --epochs 100 --learning-rate 0.001 -v data/{val_ds} \
---w-proj .3 --w-dist .3 --w-ori .3 --w-led 0 \
---model-type {model} --visible --led-inference amax"""
+--w-proj {tasks_w} --w-dist {tasks_w} --w-ori {tasks_w} --w-led {led_w} \
+--model-type {model} --visible --led-inference {led_inference} -bs 50"""
         
         if count is not None:
-            cmd += f" -c {count} -cseed 0"
+            cmd += f" -c {count} -cseed 0 --labeled-count 1"
 
         if using_pre_trained:
             cmd += f" --checkpoint-id 99 --weights-run-name {pre_trained_name}"
-        subprocess.run(cmd.strip().split(' '))
-
+#        subprocess.run(cmd.strip().split(' '))
+        testing_checkpoint = 99 if "upperbound" not in run_name else 40
         testing_cmd = f"""
-python3.11 -m testing --checkpoint-id 99 --run-name {run_name} \
+python3.11 -m testing --checkpoint-id {testing_checkpoint} --run-name {run_name} \
 --experiment-id 0 --task pose_and_led --visible \
---dataset data/{val_ds} --device {device} --inference-dump out/real_four_1000/{run_name}.pkl\
+--dataset data/{val_ds} --device {device} --inference-dump out/real_four_grid/{run_name}.pkl\
         """
         subprocess.run(testing_cmd.strip().split(' '))
 
