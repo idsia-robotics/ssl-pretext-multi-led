80a001101d9f3c5c620503420096609ff97c5ec6
Nicholas Carlotti <nicholas.carlotti@supsi.ch>
2024-03-18 16:09:10 +0100

    Added dummy predictor function

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -23,11 +23,24 @@ def angle_difference(preds, trues):
     return np.abs(custom_modulo(trues - preds + np.pi, np.pi * 2) - np.pi)
 
 
-def leds_auc(led_preds, led_trues):
+def leds_auc(led_preds, led_trues, led_visibility= None):
     total_score = 0
     scores = []
+    if led_visibility is None:
+        led_visibility = np.ones_like(led_preds, dtype=np.int64)
+
     for i in range(led_preds.shape[1]):
-        score = binary_auc(led_preds[:, i], led_trues[:, i])
+        score = binary_auc(led_preds[led_visibility[:, i], i], led_trues[led_visibility[:, i], i])
         scores.append(score)
     total_score = sum(scores) / len(scores)
     return total_score, scores
+
+
+def mean_dummy_predictor_error(data, error = 'absolute'):
+    mean = data.mean(-1)
+    if error == 'absolute':
+        error_fn = np.abs
+    elif error == 'squared':
+        error_fn = lambda x: np.linalg.norm(x, ord = 2, axis = -1)
+    
+    return error_fn(data - mean)
